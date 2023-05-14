f5ce7db33608ba9815146dc51e40f39a23728c7c
tufaelkhan <tufaelkhan247@gmail.com>
2023-05-14 10:55:58 -0700

    vercel file added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = ('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
@@ -21,6 +22,21 @@ const client = new MongoClient(uri, {
   }
 });
 
+const verifyJwt = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({error: true, message: 'unauthorized access'})
+  }
+  const token = authorization.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+    if(error){
+      return res.status(401).send({error: true, message: 'unauthorized token'})
+    }
+    req.decoded = decoded
+    next()
+  })
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -28,6 +44,16 @@ async function run() {
 
     const serviceCollection = client.db('cardoctor').collection('services');
     const bookingCollection = client.db('cardoctor').collection('bookings')
+
+    //jwt token
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      console.log(user)
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '1h'
+      })
+      res.send({token})     
+    })
 
     app.get('/services', async(req, res) => {
       const cursor = serviceCollection.find();
@@ -46,9 +72,15 @@ async function run() {
     res.send(result)
   })
 
-  //bookings
-  app.get('/bookings', async(req, res) => {
-    console.log(req.query.email)
+  //bookings routes
+  app.get('/bookings', verifyJwt, async(req, res) => {
+    // console.log(req.headers.authorization)
+    const decoded = req.decoded
+    console.log('came back after verify', decoded);
+
+    if(decoded.email !== req.query.email){
+      return res.status(403).send({error: 1, message: 'forbiden access'})
+    }
     let query = {};
     if(req.query?.email){
       query = {email: req.query.email}
